912e487eaff9ad98d211da3e0c2f9ac229ea9a13
chakri1998 <35056463+chakri1998@users.noreply.github.com>
2019-12-09 16:15:44 +0530

    Add files via upload

---
+++ company/src/test/java/com/mindtree/company/CompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.company;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
